e8910b65abb6589ccdc0fa0dfde7b4b05e9ebded
Slawomir Laskowski <laskowski.box@gmail.com>
2018-02-02 16:54:07 +0100

    Update

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mocha-webpack --webpack-config node_modules/react-app-env/config/webpack.config.test.js app/**/*.test.js
+mocha-webpack --webpack-config node_modules/react-app-env/config/webpack.config.test.js {app,src}/**/*.{test,spec}.js
