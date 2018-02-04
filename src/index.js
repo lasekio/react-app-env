06176373c7b6628d4c9ddf2231efc2067df9495c
Slawomir Laskowski <laskowski.box@gmail.com>
2018-02-04 01:24:42 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-
-const render = Component => {
-    ReactDOM.render(
-        <AppContainer>
-            <Component />
-        </AppContainer>,
-        document.getElementById('main')
-    )
-}
-const entry = _REACT_ENV_APP_ENTRYPOINT;
+import { hot } from 'react-hot-loader';
 
 const App = require(_REACT_ENV_APP_ENTRYPOINT).default;
 
-render(App);
+const HotModule = hot(module)(App)
 
-if (module.hot) {
-    console.log('accepting..', entry);
-    module.hot.accept()
-}
+ReactDOM.render(<HotModule/>, document.getElementById('main'));
