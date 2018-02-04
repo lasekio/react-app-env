236b3e2db93c18d255f46d25fa8467022ce748cd
Slawomir Laskowski <laskowski.box@gmail.com>
2018-02-04 01:30:10 +0100

    Update

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { hot } from 'react-hot-loader';
+
+const AppClient = require(_REACT_ENV_APP_ENTRYPOINT).default;
+
+
+const App = () => <AppClient/>;
+
+export default hot(module)(App)
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { hot } from 'react-hot-loader';
+import App from "../App";
 
-const App = require(_REACT_ENV_APP_ENTRYPOINT).default;
-
-const HotModule = hot(module)(App)
-
-ReactDOM.render(<HotModule/>, document.getElementById('main'));
+ReactDOM.render(<App/>, document.getElementById('main'));
